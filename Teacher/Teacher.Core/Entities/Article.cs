c98ab54495da109a12550ad6a095ad2bedfbfc8e
Ilia Siladi <alabama343@mail.ru>
2016-11-14 11:31:40 +0200

    main page/base structure

---
+++ Teacher/Teacher.Core/Entities/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Teacher.Core.Entities.Base;
+
+namespace Teacher.Core.Entities
+{
+    public class Article : EntityBase
+    {
+        public string Title { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Content { get; set; }
+
+        public virtual List<Comment> Comments { get; set; }
+    }
+}
---
+++ Teacher/Teacher.Core/Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Teacher.Core.Entities.Base;
+
+namespace Teacher.Core.Entities
+{
+    public class Comment : EntityBase
+    {
+        public string Content { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
